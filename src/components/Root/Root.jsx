41a6ce96fa10b73e30d64d50c3dfc0c22365a985
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-05 23:13:23 +0600

    header set & import

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
 
 const Root = () => {
   return (
     <div>
-        <h2>Hello Word</h2>
+        <Header></Header>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
