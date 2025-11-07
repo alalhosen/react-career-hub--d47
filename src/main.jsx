2a4a23aeb97aac46c2e196ea4a2d9c80c3ac6577
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-07 21:31:42 +0600

    add it children component

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,24 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./components/Root/Root";
 import Home from "./components/Home/Home";
+import AppliedJobs from "./components/AppliedJobs/AppliedJobs";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element:<Root></Root>,
-    children:[
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children: [
       {
-        path:'/',
-        element:<Home></Home>
-      }
-    ]
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/applied",
+        element: <AppliedJobs></AppliedJobs>,
+      },
+    ],
   },
 ]);
 
