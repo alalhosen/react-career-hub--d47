561126b3a3df25b4ad02fe7166212b92806dc8d3
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-08 23:09:32 +0600

    use useState & useEffect

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
+
 const FeaturedJobs = () => {
+    const [jobs, setJobs]=useState([]);
+    useEffect(()=>{
+        fetch('jobs.json')
+        .then(res=>res.json())
+        .then(data=>setJobs(data));
+    },[])
   return (
     <div>
       <div className="text-center">
-        <h2 className="text-5xl">Featured Jobs</h2>
+        <h2 className="text-5xl">Featured Jobs: {jobs.length}</h2>
         <p>
           Explore thousands of job opportunities with all the information you
           need. Its your future
@@ -11,5 +19,5 @@ const FeaturedJobs = () => {
     </div>
   );
 };
-
+ 
 export default FeaturedJobs;
