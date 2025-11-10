f4ccf202d21743322c72dd020d8c54b2ddd2d5f9
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-10 22:16:27 +0600

    install react icon

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,3 +1,5 @@
+import { IoLocation } from "react-icons/io5";
+
 const Job = ({ job }) => {
   const { logo, job_title, company_name, remote_or_onsite, location,job_type, salary } = job;
   return (
@@ -15,6 +17,10 @@ const Job = ({ job }) => {
           <button className="px-5 py-2 font-extrabold border rounded gorder-[#7E9FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
           <button className="px-5 py-2 font-extrabold border rounded gorder-[#7E9FE] mr-4 text-[#7E90FE]">{job_type}</button>
         </div>
+        <div>
+        <IoLocation />
+        </div>
+        {/* <div><FaLocationDot></FaLocationDot></div> */}
         <div className="card-actions">
           <button className="btn btn-primary">View Details</button>
         </div>
