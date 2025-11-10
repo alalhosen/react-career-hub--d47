089fc2d9766c08867d7225279a8abef181efb203
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-10 16:13:38 +0600

    btn add

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -11,6 +11,10 @@ const Job = ({ job }) => {
       <div className="card-body">
         <h2 className="card-title">{job_title}</h2>
         <p>{company_name}</p>
+        <div>
+          <button className="px-5 py-2 font-extrabold border rounded gorder-[#7E9FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
+          <button className="px-5 py-2 font-extrabold border rounded gorder-[#7E9FE] mr-4 text-[#7E90FE]">{job_type}</button>
+        </div>
         <div className="card-actions">
           <button className="btn btn-primary">View Details</button>
         </div>
