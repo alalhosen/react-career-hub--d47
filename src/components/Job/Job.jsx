13db34fc85d582c1fe7fbfc290562fc538972f2e
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-10 15:35:06 +0600

    setup dynamic job component

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,7 +1,7 @@
 const Job = ({ job }) => {
-  const { logo } = job;
+  const { logo, job_title, company_name, remote_or_onsite, location,job_type, salary } = job;
   return (
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+    <div className="card card-compact bg-base-100 shadow-xl">
       <figure>
         <img
           src={logo}
@@ -9,10 +9,10 @@ const Job = ({ job }) => {
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+        <h2 className="card-title">{job_title}</h2>
+        <p>{company_name}</p>
+        <div className="card-actions">
+          <button className="btn btn-primary">View Details</button>
         </div>
       </div>
     </div>
