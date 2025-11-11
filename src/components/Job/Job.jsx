d8961359f48e86dd9dbcb13270cfd5f7d8e4def1
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-11 19:44:08 +0600

    set location

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,26 +1,34 @@
-import { IoLocation } from "react-icons/io5";
+import { FaLocationDot } from "react-icons/fa6";
 
 const Job = ({ job }) => {
-  const { logo, job_title, company_name, remote_or_onsite, location,job_type, salary } = job;
+  const {
+    logo,
+    job_title,
+    company_name,
+    remote_or_onsite,
+    location,
+    job_type,
+    salary,
+  } = job;
   return (
     <div className="card card-compact bg-base-100 shadow-xl">
       <figure>
-        <img
-          src={logo}
-          alt="Shoes"
-        />
+        <img src={logo} alt="Shoes" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{job_title}</h2>
         <p>{company_name}</p>
         <div>
-          <button className="px-5 py-2 font-extrabold border rounded gorder-[#7E9FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
-          <button className="px-5 py-2 font-extrabold border rounded gorder-[#7E9FE] mr-4 text-[#7E90FE]">{job_type}</button>
+          <button className="px-5 py-2 font-extrabold border rounded gorder-[#7E9FE] mr-4 text-[#7E90FE]">
+            {remote_or_onsite}
+          </button>
+          <button className="px-5 py-2 font-extrabold border rounded gorder-[#7E9FE] mr-4 text-[#7E90FE]">
+            {job_type}
+          </button>
         </div>
         <div>
-        <IoLocation />
+          <h2 className="flex"><FaLocationDot className="text-2xl"></FaLocationDot>{location}</h2>
         </div>
-        {/* <div><FaLocationDot></FaLocationDot></div> */}
         <div className="card-actions">
           <button className="btn btn-primary">View Details</button>
         </div>
