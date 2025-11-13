e40a797453a249c246cb482f21d0e64f2ca14625
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-13 23:22:13 +0600

    use find job

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,4 +1,10 @@
+import { useLoaderData, useParams } from "react-router-dom";
+
 const JobDetails = () => {
+  const jobs = useLoaderData();
+  const { id } = useParams();
+  const job = jobs.find((job) => job.id === id);
+  console.log(id, jobs);
   return (
     <div>
       <h2>Job Details Of: </h2>
