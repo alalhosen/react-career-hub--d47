b59623c923d5d325aa0baa18ebd4ceb798350aa4
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-15 21:26:18 +0600

    something add in jobDetails

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -3,15 +3,20 @@ import { useLoaderData, useParams } from "react-router-dom";
 const JobDetails = () => {
   const jobs = useLoaderData();
   const { id } = useParams();
-  const idInt=parseInt(id);
+  const idInt = parseInt(id);
   const job = jobs.find((job) => job.id === idInt);
   console.log(jobs);
   return (
     <div>
       <h2>Job Details Of: {id}</h2>
       <div className="grid gap-4 md:grid-cols-4">
-        <div className="border md:col-span-3"></div>
-        <div className="border"></div>
+        <div className="border md:col-span-3">
+          <h2 className="text-4xl">Details coming here</h2>
+        </div>
+        <div className="border">
+          <h2 className="text-2xl">Side things</h2>
+          <button>Apply Now</button>
+        </div>
       </div>
     </div>
   );
